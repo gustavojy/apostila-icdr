9f97b28035b94d65aab96e9c92616119a106215b
Gustavo Jun Yakushiji <gustavojun023@gmail.com>
2025-01-21 15:19:05 -0300

    docs

---
+++ _options.R
@@ -0,0 +1,16 @@
+options(scipen = 999)
+
+knitr::opts_chunk$set(
+  echo = TRUE,
+  warning = FALSE, 
+  message = FALSE,
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  error = TRUE,
+  results = "markup",
+  fig.align = "center",
+  fig.asp = 0.75
+)
+
+library(tidyverse)
